0494ac82339e7f30dab65ab70d57fe70a649ea55
EgorGL00MI <gitstage0@yandex.ru>
2023-12-11 21:15:56 +0300

    feat: add cursor pointer for close icon in cartitem

--- .history/src/Components/CartItem/CartItem_20231211204120.tsx
+++ .history/src/Components/CartItem/CartItem_20231211204120.tsx
@@ -40,11 +40,10 @@ const CartItem: FC<ICartItem> = ({ elem }) => {
                     onClick={() => changeQuantity(elem, Math.max(1, elem.quantity + 1))} />
             </div>
             <span className={cl.price}>{Math.ceil((elem.price * elem.quantity))}$</span>
+
             <IoMdClose
-            style={{color: 'red',
-            width:'50px',
-            height: '50px',
-            cursor: 'pointer'}}
+            style={{color: 'red',width:'50px',
+            height: '50px'}}
                 onClick={() => removeToCart({id : elem.id})}
             />
 
