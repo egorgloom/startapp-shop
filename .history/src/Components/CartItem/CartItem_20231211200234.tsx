8ec8ca37b3f70777d02ef23ac3a293f9a84b7077
EgorGL00MI <gitstage0@yandex.ru>
2023-12-11 20:09:11 +0300

    feat: add width and height for icon delete in cartitem

--- .history/src/Components/CartItem/CartItem_20231211200234.tsx
+++ .history/src/Components/CartItem/CartItem_20231211200234.tsx
@@ -40,7 +40,7 @@ const CartItem: FC<ICartItem> = ({ elem }) => {
             </div>
             <span className={cl.price}>{Math.ceil((elem.price * elem.quantity))}$</span>
             <FaRegWindowClose
-            style={{cursor: 'pointer', width: '50px', height: '50px'}}
+            style={{cursor: 'pointer', width: '20px', height: '20px'}}
                 onClick={() => removeToCart({id : elem.id})}
             />
         </div>
